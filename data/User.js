0f2e5ee36befc4dd9763c75f219d457c4b6cb76a
DanillGodovan <bonkinans@gmail.com>
2021-01-17 04:07:25 +0300

    creating site

---
+++ data/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const config = require("../config.json")
+const schema = mongoose.Schema({
+    guildID: String,
+    userID: String,
+    messages: { type: Number, default: 0 },
+    locations: { type: Number, default: 0 },
+    verified: { type: Boolean, default: false },
+    warn: { type: Number, default: 0 },
+    points: { type: Number, default: 0 },
+    tradeText: { type: String, default: `[Текст]` },
+    _time: { type: Number, default: 0 }
+});
+module.exports = mongoose.model("User", schema)
---
+++ strategies/discordstrategy.js
@@ -0,0 +1,27 @@
+const DiscordStrategy = require('passport-discord').Strategy;
+const passport = require("passport");
+const mongoose = require('mongoose')
+const User = require('../data/User')
+require("dotenv").config();
+
+    passport.use(new DiscordStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/verified',
+    scope: ['identify', 'email']
+}, async (accessToken, refreshToken, profile, done) => {
+    console.log(profile);
+    let profileID = profile.id.toString()
+    let data = await User.findOne({userID: profileID})
+    console.log(data)
+    data.verified = true
+    data.save()
+    done(null, accessToken)
+}));
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+     done(null, id);
+  });
